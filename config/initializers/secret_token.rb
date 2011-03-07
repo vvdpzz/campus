a61a9b3c22e8536dad9e640aebe13711c60ba2c4
VvDPzZ <vvdpzz@gmail.com>
2011-03-07 13:06:55 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campus::Application.config.secret_token = '43cedcf1ddf5f981438e1f6d82d2f6f3062fbebe4ccc09a56c8904cc01d95d368039773aa244463ed031bd24d58a95cf60443c33b975e5ed33ac8b33c1908c9c'
