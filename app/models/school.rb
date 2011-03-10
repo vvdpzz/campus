020fd45024b1a3a6a93440c5564501c3f7a5c802
VvDPzZ <vvdpzz@gmail.com>
2011-03-10 21:48:31 +0800

    uniq

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,8 +3,11 @@ class School < ActiveRecord::Base
   has_many :booms, :dependent => :destroy
   
   validates_presence_of :name, :on => :create, :message => "can't be blank"
+  validates_uniqueness_of :name, :on => :create, :message => "must be unique"
   validates_presence_of :abbr, :on => :create, :message => "can't be blank"
+  validates_uniqueness_of :abbr, :on => :create, :message => "must be unique"
   validates_presence_of :website, :on => :create, :message => "can't be blank"
+  validates_uniqueness_of :website, :on => :create, :message => "must be unique"
   
   def to_param
     abbr
