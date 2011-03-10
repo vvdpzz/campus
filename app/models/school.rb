508ed9073509f46ec65b21b99302fc4c03c73fef
VvDPzZ <vvdpzz@gmail.com>
2011-03-10 21:46:10 +0800

    fix 返回

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,6 +2,10 @@ class School < ActiveRecord::Base
   
   has_many :booms, :dependent => :destroy
   
+  validates_presence_of :name, :on => :create, :message => "can't be blank"
+  validates_presence_of :abbr, :on => :create, :message => "can't be blank"
+  validates_presence_of :website, :on => :create, :message => "can't be blank"
+  
   def to_param
     abbr
   end
